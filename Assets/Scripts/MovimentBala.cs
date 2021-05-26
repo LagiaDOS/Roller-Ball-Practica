4f3345aa8fc4293fa52ffcf6f7efb7bf7ce51c10
Roger <rgrrtz@gmail.com>
2021-05-26 19:33:16 +0200

    COMMIT FINAL
    TOT ACABAT

---
+++ Assets/Scripts/MovimentBala.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimentBala : MonoBehaviour
+{
+
+
+    public Vector3 movement;
+    public float speed = 2.0f;
+
+    public int angleX;
+    public int angleY;
+    public int angleZ;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.transform.rotation = Quaternion.Euler(angleX, angleY, angleZ);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        this.transform.Translate(movement * Time.deltaTime * speed);
+    }
+}
